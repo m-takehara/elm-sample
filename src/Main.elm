39c0cbfad4c17ea1c21c8c4c622122babd8ad174
m-takehara <m.takehara.0611@gmail.com>
2021-10-21 22:59:02 +0900

    公式のボタンサンプルにメッセージを追加してみた

--- src/Main.elm
+++ src/Main.elm
@@ -4,28 +4,40 @@ import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
-main = Browser.sandbox { init = init, update = update, view = view }
-  
-type alias Model = Int
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+type alias Model =
+    { count : Int, message : String }
+
+
 init : Model
-init = 0
+init =
+    { count = 0, message = "This is empty" }
+
 
 type Msg
-  = Increment
-  | Decrement
-  
+    = Increment
+    | Decrement
+
+
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
-    Decrement ->
-      model - 1
-      
+    case msg of
+        Increment ->
+            { count = model.count + 1, message = "You increased." }
+
+        Decrement ->
+            { count = model.count - 1, message = "You decreased." }
+
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model.count) ]
+        , button [ onClick Increment ] [ text "+" ]
+        , div [] [ text model.message ]
+        ]
