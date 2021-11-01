6f3abe8816e975b50cb0b734a8291efc0625cbd4
m-takehara <m.takehara.0611@gmail.com>
2021-11-01 22:26:12 +0900

    Form のサンプル問題を解いた

--- src/Form.elm
+++ src/Form.elm
@@ -63,7 +63,11 @@ view model =
         [ viewInput "text" "Name" model.name Name
         , viewInput "password" "Password" model.password Password
         , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
-        , viewValidation model
+        , viewIsSame model.password model.passwordAgain
+        , viewInputLength model.password
+        , viewUsingLowercase model.password
+        , viewUsingUppercase model.password
+        , viewUsingNumber model.password
         ]
 
 
@@ -72,10 +76,63 @@ viewInput t p v toMsg =
     input [ type_ t, placeholder p, value v, onInput toMsg ] []
 
 
-viewValidation : Model -> Html msg
-viewValidation model =
-    if model.password == model.passwordAgain then
-        div [ style "color" "green" ] [ text "OK" ]
+viewIsSame : String -> String -> Html msg
+viewIsSame a b =
+    if a == b then
+        div [ style "color" "green" ] [ text "Password Re-enter: OK" ]
 
     else
-        div [ style "color" "red" ] [ text "Passwords do not match!" ]
+        div [ style "color" "red" ] [ text "Password Re-enter: NG" ]
+
+
+viewInputLength : String -> Html msg
+viewInputLength p =
+    div [] [ text ("password length: " ++ String.fromInt (String.length p)) ]
+
+
+viewUsingLowercase : String -> Html msg
+viewUsingLowercase model =
+    if String.length model == 0 then
+        div [ style "color" "red" ] [ text "Using Lowercase: NG" ]
+
+    else if containsLowercase model then
+        div [ style "color" "green" ] [ text "Using Lowercase: OK" ]
+
+    else
+        div [ style "color" "red" ] [ text "Using Lowercase: NG" ]
+
+
+containsLowercase : String -> Bool
+containsLowercase p =
+    List.any (\s -> String.toLower s == s) (String.split "" p)
+
+
+viewUsingUppercase : String -> Html msg
+viewUsingUppercase model =
+    if String.length model == 0 then
+        div [ style "color" "red" ] [ text "Using Uppercase: NG" ]
+
+    else if containsUppercase model then
+        div [ style "color" "green" ] [ text "Using Uppercase: OK" ]
+
+    else
+        div [ style "color" "red" ] [ text "Using Uppercase: NG" ]
+
+
+containsUppercase : String -> Bool
+containsUppercase p =
+    List.any (\s -> String.toUpper s == s) (String.split "" p)
+
+
+viewUsingNumber : String -> Html msg
+viewUsingNumber p =
+    if containsNumber p then
+        div [ style "color" "green" ] [ text "Using Number: OK" ]
+
+    else
+        div [ style "color" "red" ] [ text "Using Number: NG" ]
+
+
+containsNumber : String -> Bool
+containsNumber p =
+    List.any (\s -> List.any (\t -> s == t) (String.split "" "1234567890")) (String.split "" p)
